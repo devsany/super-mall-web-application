f754f6506fdd5945627051f14122c9eb6dc9d7f9
devsany <sunnyrajbodhgaya19@gmail.com>
2024-10-24 20:31:54 +0530

    firest commit

--- src/firebase/firebaseconsole.js
+++ src/firebase/firebaseconsole.js
@@ -1,24 +1,25 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// fire base store
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_API_AUTHDOMAIN,
-  databaseURL: import.meta.env.VITE_API_DATABASEURL,
-  projectId: import.meta.env.VITE_API_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_API_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_API_MESSAGE_SENDER_ID,
-  appId: import.meta.env.VITE_API_APP_ID,
-  measurementId: import.meta.env.VITE_API_MEASURNMENT_ID,
+  apiKey: "AIzaSyDZaCg64gmfNU9AvIJ8FpTF0UgSUnc_LhA",
+  authDomain: "super-mall-web-applicati-22e23.firebaseapp.com",
+  databaseURL:
+    "https://super-mall-web-applicati-22e23-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "super-mall-web-applicati-22e23",
+  storageBucket: "super-mall-web-applicati-22e23.appspot.com",
+  messagingSenderId: "306244265724",
+  appId: "1:306244265724:web:eb7560895ebcf8924f5979",
+  measurementId: "G-E6ZQWVGX17",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const googleProvider = new GoogleAuthProvider(app);
+export const auth = getAuth(app);
 
-export default app;
+// fire store
+export const db = getFirestore(app);
