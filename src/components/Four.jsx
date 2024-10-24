215cd055dc240db0278ec04424ba9c4bf692b75c
devsany <sunnyrajbodhgaya19@gmail.com>
2024-10-24 23:36:00 +0530

    add the toggle functionality

--- src/components/Four.jsx
+++ src/components/Four.jsx
@@ -34,75 +34,79 @@ const Four = () => {
   return (
     <div>
       <button onClick={() => setToggle(!toggle)}>Question Number 4</button>
-      <h2>Task Four</h2>
-      <h3>
-        Storing the input data with Product Name and Product Price as an object
-        into array, displaying that list data, calculating total Product Price
-        using javascript.
-      </h3>
-      <div>
-        <div>
-          <label htmlFor="product_name">Product Name *</label>
-        </div>
-        <div>
-          <input
-            className="border"
-            type="text"
-            id="product_name"
-            name="name"
-            value={productName}
-            onChange={(e) => setProductName(e.target.value)}
-          />
-        </div>
-        <div>{error ? <>{error.productName}</> : null}</div>
-      </div>
+      {toggle && (
+        <>
+          <h2>Task Four</h2>
+          <h3>
+            Storing the input data with Product Name and Product Price as an
+            object into array, displaying that list data, calculating total
+            Product Price using javascript.
+          </h3>
+          <div>
+            <div>
+              <label htmlFor="product_name">Product Name *</label>
+            </div>
+            <div>
+              <input
+                className="border"
+                type="text"
+                id="product_name"
+                name="name"
+                value={productName}
+                onChange={(e) => setProductName(e.target.value)}
+              />
+            </div>
+            <div>{error ? <>{error.productName}</> : null}</div>
+          </div>
 
-      <div>
-        <div>
-          <label htmlFor="product_price">Product Price *</label>
-        </div>
-        <div>
-          <input
-            className="border"
-            type="text"
-            id="product_price"
-            name="price"
-            value={productPrice}
-            onChange={(e) => setProductPrice(e.target.value)}
-          />
-        </div>
-        <div>{error ? <>{error.productPrice}</> : null}</div>
-      </div>
-      <button onClick={handleSubmit}>Submit</button>
-      <div>
-        <h2>Output Result:</h2>
-        <div className="flex">
-          <div className="border p-4">
-            <h3>Sale Price</h3>
-            <hr />
-            {productNameArr.map((item) => {
-              return (
-                <>
-                  {price.map((itemPrice, index) => {
-                    return (
-                      <>
-                        <li key={index}>
-                          {item}-{itemPrice}
-                        </li>
-                      </>
-                    );
-                  })}
-                </>
-              );
-            })}
+          <div>
+            <div>
+              <label htmlFor="product_price">Product Price *</label>
+            </div>
+            <div>
+              <input
+                className="border"
+                type="text"
+                id="product_price"
+                name="price"
+                value={productPrice}
+                onChange={(e) => setProductPrice(e.target.value)}
+              />
+            </div>
+            <div>{error ? <>{error.productPrice}</> : null}</div>
           </div>
-          <div className="border p-4">
-            <h3>Total Price</h3>
-            <hr />
-            {totalPrice ? <>{totalPrice}</> : <>Initial Price is 0</>}
+          <button onClick={handleSubmit}>Submit</button>
+          <div>
+            <h2>Output Result:</h2>
+            <div className="flex">
+              <div className="border p-4">
+                <h3>Sale Price</h3>
+                <hr />
+                {productNameArr.map((item) => {
+                  return (
+                    <>
+                      {price.map((itemPrice, index) => {
+                        return (
+                          <>
+                            <li key={index}>
+                              {item}-{itemPrice}
+                            </li>
+                          </>
+                        );
+                      })}
+                    </>
+                  );
+                })}
+              </div>
+              <div className="border p-4">
+                <h3>Total Price</h3>
+                <hr />
+                {totalPrice ? <>{totalPrice}</> : <>Initial Price is 0</>}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };
