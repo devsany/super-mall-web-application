108b4d2a21b5377d14c347cd9b930092c2c55978
devsany <sunnyrajbodhgaya19@gmail.com>
2024-10-28 08:45:16 +0530

    add the css and adjust the alignment of admin dashboard admin panal

--- src/components/ADMIN/AdminCreateShop.jsx
+++ src/components/ADMIN/AdminCreateShop.jsx
@@ -66,16 +66,36 @@ const AdminCreateShop = () => {
       <div className="grid grid-cols-11">
         <div className="border col-span-2">
           <div>
-            <NavLink to="/admin">OverView Admin panel</NavLink>
+            <NavLink
+              className="block pt-2 mt-1 ml-1 mr-1 pb-2 text-center border-b-2 transition-transform transform hover:scale-105 text-gray-700 hover:bg-gray-100 rounded-lg"
+              to="/admin"
+            >
+              OverView Admin panel
+            </NavLink>
           </div>
           <div>
-            <NavLink to="/admin/create_shop">Create Shop</NavLink>
+            <NavLink
+              className="block pt-2 mt-1 ml-1 mr-1 pb-2 text-center border-b-2 transition-transform transform hover:scale-105 text-gray-700 hover:bg-gray-100 rounded-lg"
+              to="/admin/create_shop"
+            >
+              Create Shop
+            </NavLink>
           </div>
           <div>
-            <NavLink to="/admin/list_of_all_shop">View all Shop</NavLink>
+            <NavLink
+              className="block pt-2 mt-1 ml-1 mr-1 pb-2 text-center border-b-2 transition-transform transform hover:scale-105 text-gray-700 hover:bg-gray-100 rounded-lg"
+              to="/admin/list_of_all_shop"
+            >
+              View all Shop
+            </NavLink>
           </div>
           <div>
-            <NavLink to="/admin/view/floor">View Floor wise shop</NavLink>
+            <NavLink
+              className="block pt-2 mt-1 ml-1 mr-1 pb-2 text-center border-b-2 transition-transform transform hover:scale-105 text-gray-700 hover:bg-gray-100 rounded-lg"
+              to="/admin/view/floor"
+            >
+              View Floor wise shop
+            </NavLink>
           </div>
         </div>
         <div className="col-span-9 border">
